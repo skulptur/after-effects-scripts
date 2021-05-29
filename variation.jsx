ebb44b8ffaf377c3e42625f5d95ae98421fcb687
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-29 00:13:22 -0300

    Update variation.jsx

--- variation.jsx
+++ variation.jsx
@@ -10,57 +10,115 @@ var scriptName = "Variation by @protobacillus";
       previewCleanup: [], // this is a list of fns to undo preview changes
       options: {
         blur: 0.5,
-        // all parameters that change size of something
-        sizing: 0.5,
-        // all parameters that change angle or direction
-        direction: 0.5,
-        // mix everything with this
-        global: 0.5,
-        // for now just colorama
-        color: 1,
-        // turbulent displace, fractal noise, cell pattern...
-        seed: 1,
-        // wave warp
-        waveType: 1,
-        // affect selected items only?
-        selectedOnly: 0,
+        sizing: 0.5, // all parameters that change size of something
+        direction: 0.5, // all parameters that change angle or direction
+        global: 0.5, // mixes everything(ish, for now)
+        color: 1, // for now just colorama
+        seed: 1, // randomize seed of turbulent displace, fractal noise, and cell pattern?
+        kaleidaMirroring: 1, // select a different mirroring option?
+        waveType: 1, // randomize wave warp?
+        selectedOnly: 0, // affect selected items only?
       },
-    };
-
-    var fx = {
-      fractalNoise: "fractalNoise",
-      cellPattern: "cellPattern",
-      turbulentDisplace: "turbulentDisplace",
-      directionalBlur: "directionalBlur",
-      colorama: "APC Colorama",
-      vectorBlur: "CC Vector Blur",
-      hexTile: "CC HexTile",
-      waveWarp: "Wave Warp",
-      ripple: "Ripple",
     };
 
     // Randomize the params
     function run() {
-      var global = state.options.global;
-      var sizing = state.options.sizing * state.options.global;
-      var direction = state.options.direction * state.options.global;
-      var color = state.options.color * state.options.global;
-      var seed = state.options.seed * state.options.global;
-      var waveType = state.options.waveType * state.options.global;
-      var blur = state.options.blur * state.options.global;
+      var mix = {
+        global: state.options.global,
+        sizing: state.options.sizing * state.options.global,
+        direction: state.options.direction * state.options.global,
+        color: state.options.color * state.options.global,
+        seed: state.options.seed * state.options.global,
+        waveType: state.options.waveType * state.options.global,
+        blur: state.options.blur * state.options.global,
+        kaleidaMirroring: state.options.kaleidaMirroring * state.options.global,
+      };
 
       state.random = xoshiro128("" + state.seed);
 
-      var randomizeSeed = getRandomizer("Random Seed", state.random);
-      var randomizePalette = getRandomizer("Use Preset Palette", state.random);
-      var randomizeAngle = getRandomizer("Angle Offset", state.random);
-      var randomizeRadius = getRandomizer("Radius", state.random);
-      var randomizeSmearing = getRandomizer("Smearing", state.random);
-      var randomizeWaveWidth = getRandomizer("Wave Width", state.random);
-      var randomizeWaveHeight = getRandomizer("Wave Height", state.random);
-      var randomizeDirection = getRandomizer("Direction", state.random);
-      var randomizeWaveType = getRandomizer("Wave Type", state.random);
-      var randomizeAmount = getRandomizer("Amount", state.random);
+      var randomize = {
+        seed: getRandomizer("Random Seed", state.random, mix.seed),
+        palette: getRandomizer("Use Preset Palette", state.random, mix.color),
+        angle: getRandomizer("Angle Offset", state.random, mix.direction),
+        radius: getRandomizer("Radius", state.random, mix.sizing),
+        smearing: getRandomizer("Smearing", state.random, mix.global),
+        waveWidth: getRandomizer("Wave Width", state.random, mix.sizing),
+        waveHeight: getRandomizer("Wave Height", state.random, mix.sizing),
+        direction: getRandomizer("Direction", state.random, mix.direction),
+        rotation: getRandomizer("Rotation", state.random, mix.direction),
+        size: getRandomizer("Size", state.random, mix.sizing),
+        waveType: getRandomizer("Wave Type", state.random, mix.waveType),
+        blurAmount: getRandomizer("Amount", state.random, mix.blur),
+        kaleidaMirroring: getRandomizer(
+          "Mirroring",
+          state.random,
+          mix.kaleidaMirroring
+        ),
+      };
+
+      var config = [
+        {
+          effect: "fractalNoise",
+          operations: [randomize.seed(0, 10000)],
+        },
+        {
+          effect: "cellPattern",
+          operations: [randomize.seed(0, 10000)],
+        },
+        {
+          effect: "turbulentDisplace",
+          operations: [randomize.seed(0, 10000)],
+        },
+        {
+          effect: "CC Kaleida",
+          operations: [
+            randomize.size(1, 1000),
+            randomize.rotation(1, 360),
+            randomize.kaleidaMirroring(1, 9),
+          ],
+        },
+        {
+          effect: "CC Griddler",
+          operations: [randomize.rotation(1, 360)],
+        },
+
+        {
+          effect: "APC Colorama",
+          operations: [randomize.palette(1, 35)],
+        },
+        {
+          effect: "CC Vector Blur",
+          operations: [
+            randomize.angle(0, 360),
+            randomize.blurAmount(-500, 500),
+          ],
+        },
+        {
+          effect: "CC HexTile",
+          operations: [randomize.radius(3, 1000), randomize.smearing(0, 100)],
+        },
+        {
+          effect: "Wave Warp",
+          operations: [
+            randomize.waveWidth(1, 1000),
+            randomize.waveHeight(-1000, 1000),
+            randomize.waveType(1, 9),
+            randomize.direction(1, 360),
+          ],
+        },
+        {
+          effect: "directionalBlur",
+          operations: [randomize.direction(1, 360)],
+        },
+        {
+          effect: "Ripple",
+          operations: [
+            randomize.radius(1, 100),
+            randomize.waveWidth(2, 100),
+            randomize.waveHeight(2, 100),
+          ],
+        },
+      ];
 
       recursiveVisit(app.project.items, function (currentItem) {
         // limit to selected items if there is any selection
@@ -75,31 +133,10 @@ var scriptName = "Variation by @protobacillus";
         forEachLayer(currentItem, function (layer) {
           var changeEffect = getChangeEffect(layer);
 
-          const cleanup = [
-            changeEffect(fx.fractalNoise, randomizeSeed(0, 10000, seed)),
-            changeEffect(fx.cellPattern, randomizeSeed(0, 10000, seed)),
-            changeEffect(fx.turbulentDisplace, randomizeSeed(0, 10000, seed)),
-
-            changeEffect(fx.colorama, randomizePalette(1, 35, color)),
-            changeEffect(fx.vectorBlur, randomizeAngle(0, 360, direction)),
-            changeEffect(fx.vectorBlur, randomizeAmount(-500, 500, blur)),
-            changeEffect(fx.hexTile, randomizeRadius(3, 1000, sizing)),
-            changeEffect(fx.hexTile, randomizeSmearing(0, 100, global)),
-            changeEffect(fx.waveWarp, randomizeWaveWidth(1, 1000, sizing)),
-            changeEffect(fx.waveWarp, randomizeWaveHeight(-1000, 1000, sizing)),
-            changeEffect(fx.waveWarp, randomizeWaveType(1, 9, waveType)),
-            changeEffect(fx.waveWarp, randomizeDirection(1, 360, direction)),
-            changeEffect(
-              fx.directionalBlur,
-              randomizeDirection(1, 360, direction)
-            ),
-            changeEffect(fx.ripple, randomizeRadius(1, 100, sizing)),
-            changeEffect(fx.ripple, randomizeWaveWidth(2, 100, sizing)),
-            changeEffect(fx.ripple, randomizeWaveHeight(2, 100, sizing)),
-          ];
-
-          forEach(cleanup, function (callback) {
-            state.previewCleanup.push(callback);
+          forEach(config, function (item) {
+            forEach(item.operations, function (operate) {
+              state.previewCleanup.push(changeEffect(item.effect, operate));
+            });
           });
         });
       });
@@ -124,14 +161,14 @@ var scriptName = "Variation by @protobacillus";
     // run first time
     run();
 
-    function getMixerChangeHandler(propName) {
+    function getMixerChangeHandler(inputName) {
       return function (value) {
-        state.options[propName] = value;
+        state.options[inputName] = value;
         update();
       };
     }
 
-    buildUI({
+    createUI({
       onNext: function () {
         update(state.seed + 1);
       },
@@ -148,44 +185,50 @@ var scriptName = "Variation by @protobacillus";
         // end undo group
         app.endUndoGroup();
       },
-      options: {
-        color: {
-          initialValue: state.options.color,
-          onChange: getMixerChangeHandler("color"),
-        },
-        sizing: {
-          initialValue: state.options.sizing,
-          onChange: getMixerChangeHandler("sizing"),
-        },
-        direction: {
-          initialValue: state.options.direction,
-          onChange: getMixerChangeHandler("direction"),
-        },
-        global: {
-          initialValue: state.options.global,
-          onChange: getMixerChangeHandler("global"),
-        },
-        seed: {
-          initialValue: state.options.seed,
-          onChange: getMixerChangeHandler("seed"),
-        },
-        waveType: {
-          initialValue: state.options.waveType,
-          onChange: getMixerChangeHandler("waveType"),
-        },
-        blur: {
-          initialValue: state.options.blur,
-          onChange: getMixerChangeHandler("blur"),
-        },
-        selectedOnly: {
-          initialValue: state.options.selectedOnly,
-          onChange: getMixerChangeHandler("selectedOnly"),
-        },
+      getUiProps: function (inputName) {
+        return {
+          initialValue: state.options[inputName],
+          onChange: getMixerChangeHandler(inputName),
+        };
       },
     });
   }
 
   main();
+
+  // UI
+
+  function createUI(props) {
+    createPanel(function (myPanel) {
+      createText(myPanel, "Random Amount", "center");
+
+      createSlider(myPanel, "Blur", props.getUiProps("blur"));
+      createSlider(myPanel, "Size", props.getUiProps("sizing"));
+      createSlider(myPanel, "Direction", props.getUiProps("direction"));
+
+      var checkboxGroup = createGroup(myPanel, "checkboxGroup");
+      createCheckbox(checkboxGroup, "Seed", props.getUiProps("seed"));
+      createCheckbox(checkboxGroup, "Wave Type", props.getUiProps("waveType"));
+      createCheckbox(checkboxGroup, "Colorama", props.getUiProps("color"));
+      createCheckbox(
+        myPanel,
+        "Kaleida Mirroring",
+        props.getUiProps("kaleidaMirroring")
+      );
+
+      createSlider(myPanel, "Global", props.getUiProps("global"));
+      createCheckbox(
+        myPanel,
+        "Selected Only",
+        props.getUiProps("selectedOnly")
+      );
+
+      var buttonsGroup = createGroup(myPanel, "buttonsGroup");
+      createButton(buttonsGroup, "Previous", props.onPrevious);
+      createButton(buttonsGroup, "Next", props.onNext);
+      createButton(buttonsGroup, "Repeat", props.onRepeat);
+    });
+  }
 
   // UTILS
 
@@ -251,8 +294,8 @@ var scriptName = "Variation by @protobacillus";
     return Math.floor(random() * (max - min + 1) + min);
   }
 
-  function getRandomizer(property, random) {
-    return function (min, max, t) {
+  function getRandomizer(property, random, t) {
+    return function (min, max) {
       return function (effect) {
         var propValue = effect.property(property).value;
         var value = Math.round(lerp(propValue, randomInt(min, max, random), t));
@@ -331,9 +374,35 @@ var scriptName = "Variation by @protobacillus";
     };
   }
 
-  // UI
+  // UI helper functions
 
-  // helper functions
+  function createPanel(createUI) {
+    var myPanel =
+      thisObj instanceof Panel
+        ? thisObj
+        : new Window("window", scriptName, undefined, {
+            resizeable: true,
+          });
+
+    myPanel.orientation = "column";
+    myPanel.alignChildren = ["center", "top"];
+    myPanel.spacing = 10;
+    myPanel.margins = 16;
+
+    createUI(myPanel);
+
+    myPanel.onResizing = myPanel.onResize = function () {
+      this.layout.resize();
+    };
+    if (myPanel instanceof Window) {
+      myPanel.center();
+      myPanel.show();
+    } else {
+      myPanel.layout.layout(true);
+      myPanel.layout.resize();
+    }
+  }
+
   function createGroup(panel, name) {
     const group = panel.add("group", undefined, { name: name });
     group.orientation = "row";
@@ -398,55 +467,5 @@ var scriptName = "Variation by @protobacillus";
     };
 
     return checkbox;
-  }
-
-  function buildUI(props) {
-    buildPanel(function (myPanel) {
-      createText(myPanel, "Random Amount", "center");
-
-      createSlider(myPanel, "Blur", props.options.blur);
-      createSlider(myPanel, "Size", props.options.sizing);
-      createSlider(myPanel, "Direction", props.options.direction);
-
-      var checkboxGroup = createGroup(myPanel, "checkboxGroup");
-      createCheckbox(checkboxGroup, "Seed", props.options.seed);
-      createCheckbox(checkboxGroup, "Wave Type", props.options.waveType);
-      createCheckbox(checkboxGroup, "Colorama", props.options.color);
-
-      createSlider(myPanel, "Global", props.options.global);
-      createCheckbox(myPanel, "Selected Only", props.options.selectedOnly);
-
-      var buttonsGroup = createGroup(myPanel, "buttonsGroup");
-      createButton(buttonsGroup, "Previous", props.onPrevious);
-      createButton(buttonsGroup, "Next", props.onNext);
-      createButton(buttonsGroup, "Repeat", props.onRepeat);
-    });
-  }
-
-  function buildPanel(createUI) {
-    var myPanel =
-      thisObj instanceof Panel
-        ? thisObj
-        : new Window("window", scriptName, undefined, {
-            resizeable: true,
-          });
-
-    myPanel.orientation = "column";
-    myPanel.alignChildren = ["center", "top"];
-    myPanel.spacing = 10;
-    myPanel.margins = 16;
-
-    createUI(myPanel);
-
-    myPanel.onResizing = myPanel.onResize = function () {
-      this.layout.resize();
-    };
-    if (myPanel instanceof Window) {
-      myPanel.center();
-      myPanel.show();
-    } else {
-      myPanel.layout.layout(true);
-      myPanel.layout.resize();
-    }
   }
 })(this);
